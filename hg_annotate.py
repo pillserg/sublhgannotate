80b1d9e48c51be9eea229005e1fcd70de4db81d5
pill <pill.sv0@gmail.com>
2013-04-28 15:18:53 +0300

    Dummy hg annotate for sublime

---
+++ hg_annotate.py
@@ -0,0 +1,33 @@
+import subprocess
+import tempfile
+import os
+
+import sublime
+import sublime_plugin
+
+
+class HgAnnotateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        file_path = self.view.file_name()
+
+        if not file_path:
+            return
+
+        proc = subprocess.Popen(
+            ['hg', 'annotate', '-vudnl', file_path],
+            stdout=subprocess.PIPE
+        )
+        result = proc.stdout.read()
+
+        if not result:
+            return
+
+        file_name = os.path.basename(file_path)
+        annotate_file_path = os.path.join(tempfile.gettempdir(), file_name)
+
+        print annotate_file_path
+
+        with open(annotate_file_path, 'w') as f:
+            f.write(result)
+
+        self.view.window().open_file(annotate_file_path)
